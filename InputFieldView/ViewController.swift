a028b8a1061c4a534aba4292592975cdc4b258fb
muukii <muukii.muukii@gmail.com>
2014-11-02 17:54:32 +0900

    Initial Commit

---
+++ InputFieldView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InputFieldView
+//
+//  Created by Muukii on 11/2/14.
+//  Copyright (c) 2014 Muukii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
